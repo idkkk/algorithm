d6af110f7721b4f0ac3acb8d7d257c6209168ed7
idkkk <idkkk@163.com>
2015-07-20 14:39:08 +0800

    增加注释

--- code/scala/NumberUtils.scala
+++ code/scala/NumberUtils.scala
@@ -1,9 +1,12 @@
 
 object NumberUtils {
+
+    // 求平均数
     def average(args: Int*): Int = {
         args.sum / args.length
     }
 
+    // 交换奇偶位
     def swapOddAndEven(args: Int*): List[Int] = {
         args.sliding(2, 2).flatMap(_.reverse).toList
     }
