0cd2c79e1bce8a2ab1c1617428d7d7a34a341508
idkkk <idkkk@163.com>
2015-07-20 16:12:33 +0800

    fibonacci数列的两种实现

--- code/scala/NumberUtils.scala
+++ code/scala/NumberUtils.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 
 object NumberUtils {
 
@@ -9,5 +10,20 @@ object NumberUtils {
     // 交换奇偶位
     def swapOddAndEven(args: Int*): List[Int] = {
         args.sliding(2, 2).flatMap(_.reverse).toList
+    }
+
+    // 斐波那契数列 (传统递归实现)
+    def fibonacci(n: BigDecimal): BigDecimal = {
+        if (n <= 0) 1 else n + fibonacci(n - 1)
+    }
+
+    // 斐波那契数列 (尾递归实现)
+    def fibonacciWithTailRec(n: BigDecimal): BigDecimal = {
+        @tailrec
+        def go(n: BigDecimal, acc: BigDecimal): BigDecimal = {
+            if (n <= 0) acc else go(n - 1, n + acc)
+        }
+
+        go(n, 1)
     }
 }
