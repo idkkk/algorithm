616724a6d67e9e22046a83e70a1a1cf4698f726e
zhangbaitong <zhangbaitong@163.com>
2015-07-20 17:02:13 +0800

    arithmetic-sequence-sum

--- code/clj/algorithm.clj
+++ code/clj/algorithm.clj
@@ -3,6 +3,12 @@
 ;;1.接近数学表达
 ;;2.懒序列
 (defn fibonacci []  
-    (map first (iterate (fn [[a b]] [b (+ a b)]) [1 1])))  
+    (map first (iterate (fn [[a b]] [b (+ a b)]) [1 1N])))
 ;;使用
-(nth (fibonacci) 66) 
+(nth (fibonacci) 100) 
+
+
+;;等差数列求和
+(defn arithmetic-sequence-sum [n] (reduce + (range (inc n))))
+;;使用
+(arithmetic-sequence-sum 100)
