d06062c77e4d9f3e1f07c812d7f54f10bccd8b38
zhangbaitong <zhangbaitong@163.com>
2015-07-20 17:35:43 +0800

    update sum

--- code/clj/algorithm.clj
+++ code/clj/algorithm.clj
@@ -12,3 +12,11 @@
 (defn arithmetic-sequence-sum [n] (reduce + (range (inc n))))
 ;;使用
 (arithmetic-sequence-sum 100)
+;;等差数列求和-公式求和
+(defn arithmetic-sequence-sum2 [n] (/ (* (+ 1 n) n) 2))
+;;使用
+(arithmetic-sequence-sum2 100)
+;;等差数列求和-公式求和-带步长100
+(defn arithmetic-sequence-sum3 [n] (/ (* (+ 1 (+ 1 (* (- n 1) 100))) n) 2))
+;;使用
+(arithmetic-sequence-sum3 4)
