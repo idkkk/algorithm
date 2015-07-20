a2718ced0ca3f3060ec35814b4eeb522c0ffc901
idkkk <idkkk@163.com>
2015-07-20 14:30:18 +0800

    实现奇偶相连的两个数进行交换

--- code/scala/NumberUtils.scala
+++ code/scala/NumberUtils.scala
@@ -1,6 +1,10 @@
 
 object NumberUtils {
-    def average(args: Int*) : Int = {
+    def average(args: Int*): Int = {
         args.sum / args.length
+    }
+
+    def swapOddAndEven(args: Int*): List[Int] = {
+        args.sliding(2, 2).flatMap(_.reverse).toList
     }
 }
