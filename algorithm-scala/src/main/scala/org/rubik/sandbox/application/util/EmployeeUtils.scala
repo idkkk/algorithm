dd1387db6db3fa64d080198af10c95a65b410424
idkkk <idkkk@163.com>
2015-08-07 12:02:35 +0800

    新增加用例

--- algorithm-scala/src/main/scala/org/rubik/sandbox/application/util/EmployeeUtils.scala
+++ algorithm-scala/src/main/scala/org/rubik/sandbox/application/util/EmployeeUtils.scala
@@ -10,16 +10,45 @@ import org.rubik.sandbox.application.model.Employee
  */
 object EmployeeUtils {
   /**
-   * 求各部门员工平均薪资.
-   * @param employees 员工集合
-   * @return 各部门平均薪资Map
+   * 各部门不同年龄段的员工数(目前只计算20-29, 30-39, 40-49区间).
+   * @param employes 员工集合
+   * @return 各部门不同年龄段的员工数
    */
-  def averageSalaries(employees: List[Employee]): Map[String, BigDecimal] = {
-    def averageSalariesOfDepartment(employeesOfDepartment: List[Employee]): BigDecimal = {
-      employeesOfDepartment.view.map(_.salary).reduce(_ + _) / employeesOfDepartment.length
-    }
-    employees.groupBy(_.department).mapValues(averageSalariesOfDepartment)
+  def countForEmployeeOfAgeRange(employes: List[Employee]): Map[String, List[(Seq[Int], Int)]] = {
+    val rangeOfAge = (20 to 49).sliding(10, 10).toList
+    employes.groupBy(_.department).mapValues(subList => {
+      for(v <- rangeOfAge;
+          data = employes.count(e => subList.head.department==e.department && v.contains(e.age)) if data!=0) yield (v, data)
+    })
   }
 
 
+  /**
+   * 不同年龄区间的平均薪资(目前只计算20-29, 30-39, 40-49区间).
+   * @param employes 员工集合
+   * @return 不同年龄区间的平均薪资
+   */
+  def averageSalariesOfAgeRange(employes: List[Employee]): List[(Seq[Int], BigDecimal)] = {
+    val rangeOfAge = (20 to 49).sliding(10, 10).toList
+    for(v <- rangeOfAge) yield (v, averageSalariesOfEmployes(employes.filter(e => v.contains(e.age))))
+  }
+
+  /**
+   * 各部门员工平均薪资由高到低排序.
+   * @param employes 员工集合
+   * @return 各部门平均薪资排序
+   */
+  def averageSalariesOfDepartment(employes: List[Employee]): Seq[(String, BigDecimal)] = {
+    employes.groupBy(_.department).mapValues(averageSalariesOfEmployes).toSeq.sortBy(_._2).reverse
+  }
+
+  // ================================================================================  priavte
+  /**
+   * 获取特定员工集合的平均薪资.
+   * @param employes 员工集合
+   * @return 平均薪资
+   */
+  private def averageSalariesOfEmployes(employes: List[Employee]): BigDecimal = {
+    employes.map(_.salary).reduce(_ + _) / employes.length
+  }
 }
--- algorithm-scala/src/test/scala/org/rubik/sandbox/application/util/EmployeeUtilsSpec.scala
+++ algorithm-scala/src/test/scala/org/rubik/sandbox/application/util/EmployeeUtilsSpec.scala
@@ -17,16 +17,16 @@ class EmployeeUtilsSpec extends FlatSpec with BeforeAndAfter {
     data.append(Employee("HZ224731", "夏清寿", 37, 12000, "市场部"))
     data.append(Employee("HZ325137", "聂运民", 38, 20000, "技术部"))
     data.append(Employee("HZ332569", "袁丽玲", 25, 18000, "人力资源"))
-    data.append(Employee("GZ550020", "张涛", 32, 25000, "技术部"))
+    data.append(Employee("GZ550020", "张涛", 32, 30000, "技术部"))
     data.append(Employee("JX332300", "王义军", 36, 15000, "技术部"))
     data.append(Employee("JX332301", "李如钢", 36, 16000, "技术部"))
     data.append(Employee("JX332302", "陈牡蓉", 25, 16000, "人力资源"))
     data.append(Employee("JX332303", "唐建宇", 35, 16000, "财务部"))
     data.append(Employee("SZ518119", "刘颖刚", 36, 20000, "法务部"))
     data.append(Employee("GZ510003", "黎明", 36, 15000, "销售部"))
-    data.append(Employee("BJ100102", "宋崇峰", 34, 20000, "技术部"))
+    data.append(Employee("BJ100102", "宋崇峰", 30, 20000, "技术部"))
     data.append(Employee("BJ112200", "余竞", 35, 22000, "技术部"))
-    data.append(Employee("BJ101120", "王晓明", 36, 30000, "技术部"))
+    data.append(Employee("BJ101120", "王晓明", 36, 25000, "技术部"))
     data.append(Employee("BJ101132", "华永兵", 37, 20000, "技术部"))
     data.append(Employee("JX332305", "梅雪飞", 35, 12000, "人力资源"))
     data.append(Employee("JX332306", "徐杨", 36, 12000, "人力资源"))
@@ -40,7 +40,7 @@ class EmployeeUtilsSpec extends FlatSpec with BeforeAndAfter {
     data.append(Employee("GZ510003", "魏小恒", 36, 30000, "技术部"))
     data.append(Employee("BJ100152", "马欣", 38, 32000, "技术部"))
     data.append(Employee("BJ100142", "王菲", 32, 30000, "技术部"))
-    data.append(Employee("BJ100056", "池大大", 38, 90000, "技术部"))
+    data.append(Employee("BJ100056", "Mac", 40, 90000, "技术部"))
     data.append(Employee("BJ123102", "戴成", 32, 30000, "市场部"))
   }
 
@@ -49,6 +49,14 @@ class EmployeeUtilsSpec extends FlatSpec with BeforeAndAfter {
   }
 
   "the department" should "average salaries" in {
-    assert(EmployeeUtils.averageSalaries(data.toList) == Map("法务部" -> 20000, "组织部" -> 31000, "人力资源" -> 13750, "市场部" -> 20000, "财务部" -> 16000, "技术部" -> 28000, "销售部" -> 22500))
+    assert(EmployeeUtils.averageSalariesOfDepartment(data.toList) == Seq(("组织部",31000), ("技术部",28000), ("销售部",22500), ("市场部",20000), ("法务部",20000), ("财务部",16000), ("人力资源",13750)))
+  }
+
+  "employees's age range" should "average salaries" in {
+    assert(EmployeeUtils.averageSalariesOfAgeRange(data.toList) == List((Vector(20, 21, 22, 23, 24, 25, 26, 27, 28, 29),15250), (Vector(30, 31, 32, 33, 34, 35, 36, 37, 38, 39),21500), (Vector(40, 41, 42, 43, 44, 45, 46, 47, 48, 49),90000)))
+  }
+
+  "the department's age range" should "employee numbers" in {
+    assert(EmployeeUtils.countForEmployeeOfAgeRange(data.toList) == Map("法务部" -> List((Vector(30, 31, 32, 33, 34, 35, 36, 37, 38, 39),1)), "组织部" -> List((Vector(30, 31, 32, 33, 34, 35, 36, 37, 38, 39),2)), "人力资源" -> List((Vector(20, 21, 22, 23, 24, 25, 26, 27, 28, 29),4), (Vector(30, 31, 32, 33, 34, 35, 36, 37, 38, 39),4)), "市场部" -> List((Vector(30, 31, 32, 33, 34, 35, 36, 37, 38, 39),4)), "财务部" -> List((Vector(30, 31, 32, 33, 34, 35, 36, 37, 38, 39),1)), "技术部" -> List((Vector(30, 31, 32, 33, 34, 35, 36, 37, 38, 39),14), (Vector(40, 41, 42, 43, 44, 45, 46, 47, 48, 49),1)), "销售部" -> List((Vector(30, 31, 32, 33, 34, 35, 36, 37, 38, 39),2))))
   }
 }
