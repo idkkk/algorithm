45cfc616dc2f72a7f5e79f5b1879fc1a9b1abe89
idkkk <idkkk@163.com>
2015-08-12 17:17:10 +0800

    修改测试用例

--- algorithm-scala/src/test/scala/org/rubik/sandbox/common/util/FileUtilsSpec.scala
+++ algorithm-scala/src/test/scala/org/rubik/sandbox/common/util/FileUtilsSpec.scala
@@ -7,16 +7,18 @@ import scala.sys.process.Process
 class FileUtilsSpec extends FlatSpec {
 
   "reverse file" should "head line at source file equals last line at target file" in {
-    val sourceFile = "/Users/xiajinxin/Desktop/bigfile2.txt"
-    val targetFile = "/Users/xiajinxin/Desktop/target.txt"
-    FileUtils.split(sourceFile)
-    FileUtils.merge(targetFile)
+    SystemUtils.elapsedTime {
+      val sourceFile = "/Users/xiajinxin/Desktop/res.txt"
+      val targetFile = "/Users/xiajinxin/Desktop/target.txt"
+      FileUtils.split(sourceFile)
+      FileUtils.merge(targetFile)
+    }
 
-    val headOfSourceFile = "head -n 1 /Users/xiajinxin/Desktop/bigfile2.txt"
+    val headOfSourceFile = "head -n 1 /Users/xiajinxin/Desktop/res.txt"
     val tailOfTargetFile = "tail -n 1 /Users/xiajinxin/Desktop/target.txt"
 
     val headCmd = Process(Seq("/bin/bash", "-c", headOfSourceFile))
-    val tailCmd = Process(Seq("/bin/bash", "-c", headOfSourceFile))
+    val tailCmd = Process(Seq("/bin/bash", "-c", tailOfTargetFile))
     assert((headCmd !!) == (tailCmd !!))
   }
 }
