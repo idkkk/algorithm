811eedfa974806a9753c47b039b6deae05c14543
idkkk <idkkk@163.com>
2015-08-12 18:50:58 +0800

    重构代码

--- algorithm-scala/src/main/scala/org/rubik/sandbox/common/util/FileUtils.scala
+++ algorithm-scala/src/main/scala/org/rubik/sandbox/common/util/FileUtils.scala
@@ -1,7 +1,6 @@
 package org.rubik.sandbox.common.util
 
 import java.io.File
-import java.io.PrintWriter
 
 import com.google.common.base.{Strings, Charsets}
 import com.google.common.io.Files
@@ -15,39 +14,40 @@ import scala.sys.process._
  * @since 2015-08-11
  */
 object FileUtils {
-  /**
-   * 切割后的文件存储的临时目录
-   */
-  private val TEMP_DIR = "/Users/xiajinxin/Desktop/data/"
 
   /**
    * 按行切割成小文件(实现：split -l 2000 -d 3 FILE_NAME).
    * @param sourcePath 原文件全路径
    * @param lines 切割的行数
    * @param spanLength 小文件名长度
-   * @return
+   * @param dataDir 切割后的文件存储的临时目录
+   * @return 切割后的小文件存储目录
    */
-  def split(sourcePath: String, lines: Int = 2000, spanLength: Int = 3): Unit = {
+  def split(sourcePath: String, lines: Int = 50000, spanLength: Int = 3, dataDir: String = "/Users/xiajinxin/Desktop/data/"): String = {
     lazy val source = Source.fromFile(sourcePath)
     try {
       var i = 1
-      for (block <- source.getLines.sliding(lines, lines).toStream) {
-        val content = block.view.reverse.mkString(System.getProperty("line.separator"))
-        val file = new File(TEMP_DIR + Strings.padStart(i.toString, spanLength, '0'))
-        Files.write(content, file, Charsets.UTF_8)
-        i += 1
-      }
+      source.getLines.sliding(lines, lines).toStream.map( block => {
+          val content = block.view.reverse.mkString(System.getProperty("line.separator"))
+          val file = new File(dataDir + Strings.padStart(i.toString, spanLength, '0'))
+          Files.write(content, file, Charsets.UTF_8)
+          i += 1
+        }
+      )
     } finally {
       source.close()
     }
+
+    dataDir
   }
 
   /**
    * 按文件名倒序合并生成新文件(实现：find * | sort -r | xargs cat > target.txt).
+   * @param dataDir 合并源文件目录
    * @param targetPath 目标文件全路径
    */
-  def merge(targetPath: String): Unit = {
-    val cmd = s"cd ${TEMP_DIR}; find * | sort -r | xargs cat"
+  def merge(dataDir: String, targetPath: String): Unit = {
+    val cmd = s"cd ${dataDir}; find * | sort -r | xargs cat"
     val ret = Process(Seq("/bin/bash", "-c", cmd))
     ret #> new File(targetPath) !
   }
--- algorithm-scala/src/test/scala/org/rubik/sandbox/common/util/FileUtilsSpec.scala
+++ algorithm-scala/src/test/scala/org/rubik/sandbox/common/util/FileUtilsSpec.scala
@@ -2,20 +2,21 @@ package org.rubik.sandbox.common.util
 
 import org.scalatest._
 
+import org.rubik.sandbox.common.util.FileUtils._
 import scala.sys.process.Process
 
 class FileUtilsSpec extends FlatSpec {
 
   "reverse file" should "head line at source file equals last line at target file" in {
+    val sourceFile = "/Users/xiajinxin/Desktop/app.log"
+    val targetFile = "/Users/xiajinxin/Desktop/target.txt"
+
     SystemUtils.elapsedTime {
-      val sourceFile = "/Users/xiajinxin/Desktop/res.txt"
-      val targetFile = "/Users/xiajinxin/Desktop/target.txt"
-      FileUtils.split(sourceFile)
-      FileUtils.merge(targetFile)
+      merge(split(sourceFile), targetFile)
     }
 
-    val headOfSourceFile = "head -n 1 /Users/xiajinxin/Desktop/res.txt"
-    val tailOfTargetFile = "tail -n 1 /Users/xiajinxin/Desktop/target.txt"
+    val headOfSourceFile = s"head -n 1 ${sourceFile}"
+    val tailOfTargetFile = s"tail -n 1 ${targetFile}"
 
     val headCmd = Process(Seq("/bin/bash", "-c", headOfSourceFile))
     val tailCmd = Process(Seq("/bin/bash", "-c", tailOfTargetFile))
